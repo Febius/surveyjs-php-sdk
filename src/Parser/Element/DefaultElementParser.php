9cfce4126f3c72b694cd6b6151ff9cb76fdbc285
Fabio Mosca <fabio.mosca@drivek.com>
2019-10-24 13:37:54 +0200

    refactoring abstractions

--- src/Parser/Element/DefaultElementParser.php
+++ src/Parser/Element/DefaultElementParser.php
@@ -2,12 +2,28 @@
 
 namespace SurveyJsPhpSdk\Parser\Element;
 
+use SurveyJsPhpSdk\Model\Element\ElementAbstract;
 use SurveyJsPhpSdk\Model\Element\ElementInterface;
 
 class DefaultElementParser extends ElementParserAbstract
 {
-    public function parse(ElementInterface $element, \stdClass $data): ElementInterface
+    public function configure(ElementInterface $element, \stdClass $data): ElementInterface
     {
-        return $this->configure($element, $data);
+        /** @var ElementAbstract $element */
+        $element = parent::configure($element, $data);
+
+        if (isset($data->title)) {
+            $element->setTitle($data->title);
+        }
+
+        if (isset($data->isRequired)) {
+            $element->setRequired($data->isRequired);
+        }
+
+        if (isset($data->enableIf)) {
+            $element->setEnableIf($data->enableIf);
+        }
+
+        return $element;
     }
 }
--- src/Parser/SurveyResultParser.php
+++ src/Parser/SurveyResultParser.php
@@ -5,7 +5,6 @@ namespace SurveyJsPhpSdk\Parser;
 
 use SurveyJsPhpSdk\Exception\InvalidSurveyResultException;
 use SurveyJsPhpSdk\Model\Element\ElementAbstract;
-use SurveyJsPhpSdk\Model\SurveyElementModel;
 use SurveyJsPhpSdk\Model\PageModel;
 use SurveyJsPhpSdk\Model\ResultModel;
 use SurveyJsPhpSdk\Model\TemplateModel;
@@ -22,24 +21,24 @@ class SurveyResultParser
      */
     public function parse(TemplateModel $survey, string $jsonResults): array
     {
-//        $resultsModels = [];
-//
-//        $results = (array)json_decode($jsonResults);
-//
-//        foreach($results as $question => $result){
-//            $resultModel = new SurveyResultModel();
-//
-//            $resultModel->setQuestion($question);
-//            $resultModel->setAnswer($result);
-//
-//            if(!self::validateResult($survey->getPages(), $resultModel)) {
-//                throw new InvalidSurveyResultException();
-//            }
-//
-//            $resultsModels[] = $resultModel;
-//        }
-//
-//        return $resultsModels;
+        $resultsModels = [];
+
+        $results = (array)json_decode($jsonResults);
+
+        foreach($results as $question => $result){
+            $resultModel = new ResultModel();
+
+            $resultModel->setQuestion($question);
+            $resultModel->setAnswer($result);
+
+            if(!self::validateResult($survey->getPages(), $resultModel)) {
+                throw new InvalidSurveyResultException();
+            }
+
+            $resultsModels[] = $resultModel;
+        }
+
+        return $resultsModels;
     }
 
     /**
@@ -50,20 +49,18 @@ class SurveyResultParser
      */
     private function validateResult(array $pages, ResultModel $result): bool
     {
-//        /**
-//         * @var SurveyPageModel $page
-//        */
-//        foreach($pages as $page){
-//            /**
-//             * @var AbstractSurveyElementModel $element
-//            */
-//            foreach($page->getElements() as $element){
-//                if($element->isValidResult($result)) {
-//                    return true;
-//                }
-//            }
-//        }
-//
-//        return false;
+        /**
+         * @var PageModel $page
+         */
+        foreach($pages as $page){
+
+            foreach($page->getElements() as $element){
+                if($element instanceOf ElementAbstract && $element->isValidResult($result)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
